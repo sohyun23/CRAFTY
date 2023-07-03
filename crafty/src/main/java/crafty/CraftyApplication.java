52fc02663d803800ff5a827c7679dac52d1d9bdb
DESKTOP-G3C5LEN\win10 <error071312@gmail.com>
2023-07-03 15:45:12 +0900

    test : create crafty project

---
+++ crafty/src/main/java/crafty/CraftyApplication.java
@@ -0,0 +1,13 @@
+package crafty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftyApplication.class, args);
+	}
+
+}
