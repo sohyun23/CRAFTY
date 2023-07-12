0fea7406924d66566c78ef6aae5c6854a3694893
SoHyunCho <108653282+sohyun23@users.noreply.github.com>
2023-07-12 13:33:51 +0900

    Update MemberController.java

--- src/main/java/crafty/controller/MemberController.java
+++ src/main/java/crafty/controller/MemberController.java
@@ -17,67 +17,71 @@ import crafty.dto.Member;
  * */
 
 @Controller
-@RequestMapping("/member")
+//@RequestMapping("member")
 public class MemberController {
 
-	// 멤버 서비스 만들면 주석 제거
-	@Autowired
-//	private MemberService memberService;
-	
-	@GetMapping("/signUp")
-	public String signUpFornm() {
-		return "signUp";
-	}
-	
-	// 이 부분도 멤버 서비스, Dto 만들면 주석 삭제
-	@PostMapping("/signUp")
-	public String signUp(@ModelAttribute("member") Member member) {
-		// 회원 가입 로직 구현
-//		memberService.signUp(member);
-		
-		return "login";  // 회원가입 성공 시, 로그인 페이지로 이동
-	}
-	
-	@PostMapping("/find")
-	public String findMember(@RequestParam("id") String id, @RequestParam("password") String password) {
-		// ID, PW 찾기 로직 구현
-		// id, password를 이용한 검색 수행
-		return "searchResult"; // 찾기한 결과 반환 id는 alert("id"), pw는 이메일로 재설정이 가능한 링크 전송
-	}
-	
-	@PostMapping("/{id}/likes")
-    public String likeMember(@PathVariable("id") Long id) {
+    @Autowired
+//    private MemberService memberService;
+    
+    @GetMapping(value ="/signUp")
+    public String signUpForm() {
+        return "signUp";
+    }
+    
+    // 이 부분도 멤버 서비스, Dto 만들면 주석 삭제
+    @PostMapping(value = "/signUp")
+    public String signUp(@ModelAttribute("member_id") Member member) {
+        // 회원 가입 로직 구현
+//        memberService.signUp(member);
+        
+        return "login";  // 회원가입 성공 시, 로그인 페이지로 이동
+    }
+    
+//    ID,PW는 비동기로 프론트 단에서 구현
+    @PostMapping(value = "/find")
+    public String findMember(@RequestParam("login_id") String login_id, @RequestParam("login_pw") String login_pw) {
+        // ID, PW 찾기 로직 구현
+        // id, password를 이용한 검색 수행
+        return "login"; // 찾기한 결과 반환 id는 alert(), pw는 이메일로 재설정이 가능한 링크 전송
+    }
+    
+    @PostMapping(value = "/likes")
+    public String likeMember(@PathVariable("member_id") Long id) {
         // 좋아요 기능 로직 구현
         // id를 이용하여 해당 멤버에 좋아요 처리
-        return "redirect:/member/profile/" + id;
+        return "redirect:/goods/likes";
     }
 
-    @GetMapping("/{id}/alarms")
-    public String showAlarmSettings(@PathVariable("id") Long id) {
-        // 알림 설정 페이지 로직 구현
-        // id를 이용하여 해당 멤버의 알림 설정 페이지 반환
-        return "alarmSettings";
+    @GetMapping(value = "/goods/alarm")
+    public String showAlarmSettings(@PathVariable("member_id") Long id) {
+        // 알림 확인 페이지 로직 구현
+        // id를 이용하여 해당 멤버의 알림 확인 페이지 반환
+        return "redirect:/goods/alarm";
     }
-	
+    
 //    프로필, 프로필 수정
-    @GetMapping("/profile/{id}")
-    public String showProfile(@PathVariable("id") Long id, Model model) {
+    @GetMapping(value = "/profile")
+    public String showProfile(@PathVariable("member_id") Long id, Model model) {
 //        Member member = memberService.getMemberById(id);
 //        model.addAttribute("member", member);
         return "profile";
     }
 
-    @GetMapping("/profile/{id}/edit")
-    public String showProfileEditForm(@PathVariable("id") Long id, Model model) {
+    @GetMapping(value = "/profile/edit")
+    public String showProfileEditForm(@PathVariable("member_id") Long id, Model model) {
+//        // 프로필 수정 페이지
 //        Member member = memberService.getMemberById(id);
 //        model.addAttribute("member", member);
-        return "profileEdit";
+        return "/profile/edit";
+//      crafty/profileEdit
     }
 
-    @PostMapping("/profile/{id}/edit")
-    public String editProfile(@PathVariable("id") Long id, @ModelAttribute("member") Member editedMember) {
-//        memberService.updateMember(id, editedMember);
-    	
-        return "redirect:/member/profile/" + id;
+    @PostMapping(value = "/profile/edit")
+    public String editProfile(@PathVariable("member_id") Long id, @ModelAttribute("member") Member memberUpdatedAt) {
+        // 프로필 수정 후 다시 프로필 페이지로 이동 
+//        memberService.updateMember(id, memberUpdatedAt);
+        
+        return "redirect:/profile";
+//      crafty/profile/{id}
     }
 }
